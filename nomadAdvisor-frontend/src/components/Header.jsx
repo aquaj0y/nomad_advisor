76836da089504ca05b02b6e2dfee5abc28541e35
joy <67973116+aquaj0y@users.noreply.github.com>
2024-04-09 00:51:50 +0100

    fix CityDetails page route, fix Home page tags render

--- nomadAdvisor-frontend/src/components/Header.jsx
+++ nomadAdvisor-frontend/src/components/Header.jsx
@@ -1,10 +1,9 @@
 import Logo from '../assets/logo.png'
-import { CiSearch } from "react-icons/ci";
+import { IoSearchOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 
 // MUI
 import MenuHeader from './MenuHeader';
-
 
 
 export default function Header() {
@@ -17,9 +16,7 @@ export default function Header() {
         </Link>
         <h3 className='explore'>Explore</h3>
       </div>
-
-      <CiSearch id='search-icon' size={30} />
-
+      <IoSearchOutline size={30} />
       {/* <MenuHeader /> */}
     </div>
   )
