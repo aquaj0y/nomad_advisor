74a494d2c05e7a456b36c9ff63632a8aef74f26e
joy <67973116+aquaj0y@users.noreply.github.com>
2024-04-10 00:14:19 +0100

    city details done

--- nomadAdvisor-frontend/src/components/CityDetails.jsx
+++ nomadAdvisor-frontend/src/components/CityDetails.jsx
@@ -6,6 +6,11 @@ import axios, { all } from 'axios'
 import { Link } from 'react-router-dom'
 import Button from '@mui/material/Button';
 
+// MUI List
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 // MUI Box
 import { Box, ThemeProvider } from '@mui/material';
 
@@ -13,46 +18,62 @@ import { Box, ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 
+// API Keys 
 const googleAPI = import.meta.env.VITE_GOOGLE_API_KEY
+const weatherAPI = import.meta.env.VITE_WEATHER_API_KEY
 
 export default function CityDetails() {
-  // let { name } = useParams()
   let { id } = useParams()
   const [city, setCity] = useState('')
+  const [neighborhood, setNeighborhood] = useState('')
   const [cityTags, setCityTags] = useState([])
-  
+  const [temp_c, setTemp_c] = useState('')
+  const [population, setPopulation] = useState('')
+  const [countryCode, setCountryCode] = useState('')
+  const [callingCode, setCallingCode] = useState('')
+  const [currency, setCurrency] = useState('')
+  let stop = false
+
   useEffect(() => {
     const getCityDetails = async () => {
-      let response = await axios.get(`http://127.0.0.1:8000/cities/${id}`)
-      
-      // let response = await axios.get(`http://127.0.0.1:8000/cities/`)
+      console.log('Fetching city details...');
+      const cityResponse = await axios.get(`http://127.0.0.1:8000/cities/${id}`);
+      const cityData = cityResponse.data;
+      setCity(cityData);
+      setNeighborhood(cityData.neighborhood);
+      setCityTags(cityData.review.flatMap(review => review.tags).flat());
   
-      // response.data.filter()
-      setCity(response.data)
-      
-      let allTags = response.data.review.flatMap(review => (review.tags.flatMap(tag => tag)))
-      // setCityTags([...new Set(allTags)])
-      setCityTags(allTags)
-
-      console.log(response.data)
-      console.log(allTags)
-      console.log('cityTags is', cityTags)
-    }
+      if (cityData.name) {
+        console.log(`Fetching weather for ${cityData.name}...`);
+        const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${cityData.name}&aqi=no`);
+        setTemp_c(weatherResponse.data.current.temp_c);
   
-    getCityDetails()
-    
-  }, [id])
-
-  const getWeather = async () => {
-    let response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}&aqi=no`)
-
-    console.log(response)
-  }
+        console.log(`Fetching geo data for ${cityData.name}...`);
+        const geoResponse = await axios.get(`http://geodb-free-service.wirefreethought.com/v1/geo/places?limit=5&offset=0&namePrefix=${cityData.name}`);
+        if (geoResponse.data.data.length > 0) {
+          const geoData = geoResponse.data.data[0];
+          setPopulation(geoData.population);
+          setCountryCode(geoData.countryCode);
+  
+          if (geoData.countryCode) {
+            console.log(`Fetching country data for ${geoData.countryCode}...`);
+            const countryResponse = await axios.get(`http://geodb-free-service.wirefreethought.com/v1/geo/countries/${geoData.countryCode}`);
+            const countryData = countryResponse.data.data;
+            setCallingCode(countryData.callingCode);
+            setCurrency(countryData.currencyCodes[0]);
+            stop = true
+          }
+        }
+      }
+    };
+  
+    getCityDetails().catch(console.error);
+  }, [stop]);
 
   return (
     <div>
       {/* SWIPER */}
-      {console.log('cityTags here', cityTags)}
+      {/* {console.log('cityTags here', cityTags)} */}
       <div className='details-container'>
 
         <React.Fragment>
@@ -62,7 +83,75 @@ export default function CityDetails() {
             <h2 className='details-subtitle'>{city.country}</h2>
             
             <div>
-            {/* Data */}
+              {/* Data */}
+              {/* <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {[1, 2, 3].map((value) => (
+                  <ListItem
+                    key={value}
+                    disableGutters
+                  >
+                    <ListItemText primary={`Line item ${value}`} />
+                    <ListItemText secondary={`Line item ${value}`} />
+                  </ListItem>
+                ))}
+              </List> */}
+              <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {/* Weather */}
+                <ListItem
+                  // key={value}
+                  disableGutters
+                >
+                  <ListItemText primary={`Temperature`} />
+                  <ListItemText secondary={temp_c} />
+                </ListItem>
+                
+                {/* CountryCode */}
+                <ListItem
+                  // key={value}
+                  disableGutters
+                >
+                  <ListItemText primary={`Country Code`} />
+                  <ListItemText secondary={countryCode} />
+                </ListItem>
+
+                 {/* Population */}
+                 <ListItem
+                  // key={value}
+                  disableGutters
+                >
+                  <ListItemText primary={`Population`} />
+                  <ListItemText secondary={population} />
+                </ListItem>
+
+                {/* Internet Speed */}
+                <ListItem
+                  // key={value}
+                  disableGutters
+                >
+                  <ListItemText primary={`Internet Speed`} />
+                  <ListItemText secondary={`293`} />
+                </ListItem>
+                
+                {/* Currency */}
+                <ListItem
+                  // key={value}
+                  disableGutters
+                >
+                  <ListItemText primary={`Currency`} />
+                  <ListItemText secondary={currency} />
+                </ListItem>
+
+                {/* Calling Code */}
+                <ListItem
+                  // key={value}
+                  disableGutters
+                >
+                  <ListItemText primary={`Calling Code`} />
+                  <ListItemText secondary={callingCode} />
+                </ListItem>
+
+
+              </List>
             </div>
             
             <h2 className='details-subtitle'>Why We Love Here</h2>
@@ -74,7 +163,7 @@ export default function CityDetails() {
                 <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{tag}</span>))}
             </div>
 
-          <h2>Best Nomad Neighborhood</h2>
+          <h2 className='details-subtitle'>Best Nomad Neighborhood</h2>
           {/* Google Maps Embed */}
           <iframe
             width="600"
@@ -83,7 +172,7 @@ export default function CityDetails() {
             loading="lazy"
             allowFullScreen
             referrerPolicy="no-referrer-when-downgrade"
-            src={`https://www.google.com/maps/embed/v1/place?key=${googleAPI}&q=Roma Nte.`}
+            src={`https://www.google.com/maps/embed/v1/place?key=${googleAPI}&q=${neighborhood}`}
             >
           </iframe>
           <h2>Co-working Space</h2>
