f880edf72f2f140ad3350217b6a6bc00ce05700e
joy <67973116+aquaj0y@users.noreply.github.com>
2024-04-09 16:44:58 +0100

    add neighborhood successful

--- backend/nomadAdvisor/models.py
+++ backend/nomadAdvisor/models.py
@@ -1,6 +1,6 @@
+# models.py
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
-
 
 # Create your models here.
 class City(models.Model):
@@ -9,6 +9,7 @@ class City(models.Model):
   image = models.CharField(default = '')
   like = models.IntegerField(default = 0)
   dislike = models.IntegerField(default = 0)
+  neighborhood = models.CharField(max_length=100)
 
   def __str__(self):
     return self.name
@@ -16,7 +17,7 @@ class City(models.Model):
 class Review(models.Model):
   city = models.ForeignKey('City', on_delete = models.CASCADE, related_name='review')
   user = models.ForeignKey('User', on_delete = models.CASCADE, related_name='review')
-  tags = ArrayField(models.CharField(max_length=50, blank=True), default=list)
+  tags = ArrayField(models.CharField(max_length=200, blank=False))
   description = models.CharField(max_length=1000, blank=True)
 
   def __str__(self):
