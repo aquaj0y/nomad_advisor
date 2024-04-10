e030b1a1298bd5849dafc4a3a87e0c741644657a
joy <67973116+aquaj0y@users.noreply.github.com>
2024-04-10 11:49:17 +0100

    add 4 collection in explore page

--- nomadAdvisor-frontend/src/components/ExploreCollections.jsx
+++ nomadAdvisor-frontend/src/components/ExploreCollections.jsx
@@ -1,7 +1,70 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+
+import beachesBg from '../assets/beaches.jpeg'
+import citiesBg from '../assets/cities.jpeg'
+import castlesBg from '../assets/castles.jpeg'
+import mountainsBg from '../assets/mountains.jpeg'
+
+
+
 export default function ExploreCollections() {
   return (
     <div>
-      This is ExploreCollections Component
+      <h1 className='page-title'>Discover Something New</h1>
+        
+      <Box
+        className='collection-containter'
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'wrap',
+          '& > :not(style)': {
+            m: 1,
+            width: '60%',
+            height: '30vh',
+          },
+        }}
+      >
+        {/* <Container maxWidth={'xl'}> */}
+        <Paper
+          // justifyContent="center"
+          // alignItems="center"
+          sx={{
+            backgroundImage: `url(${beachesBg})`,
+            backgroundPosition: 'center'
+        }}
+        >
+          <h2 className='section-title'>Beaches</h2>
+        </Paper>
+        <Paper
+          sx={{
+            backgroundImage: `url(${citiesBg})`,
+            backgroundPosition: 'center'
+        }}
+        >
+          <h2 className='section-title'>Cities</h2>
+        </Paper>
+        <Paper
+          sx={{
+            backgroundImage: `url(${castlesBg})`,
+            backgroundPosition: 'center'
+        }}
+        >
+          <h2 className='section-title'>Castles</h2>
+        </Paper>
+        <Paper
+          sx={{
+            backgroundImage: `url(${mountainsBg})`,
+            backgroundPosition: 'center'
+        }}
+        >
+          <h2 className='section-title'>Mountains</h2>
+        </Paper>
+        {/* </Container> */}
+      </Box>
     </div>
   )
 }
