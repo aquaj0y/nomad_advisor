c49be6a2e39b1d630e6504d821e4eefb4ce4dd60
joy <67973116+aquaj0y@users.noreply.github.com>
2024-04-11 01:48:37 +0100

    styling for homepage, explorepage, explorecollection

--- nomadAdvisor-frontend/src/components/Home.jsx
+++ nomadAdvisor-frontend/src/components/Home.jsx
@@ -10,7 +10,7 @@ import summerBg from '../assets/summer.jpeg'
 import fallBg from '../assets/fall.jpeg' 
 import winterBg from '../assets/winter.jpeg'
 
-// MUI MediaCard
+// MUI 
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -18,9 +18,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-
-// MUI
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
 
@@ -54,50 +52,42 @@ export default function Home() {
 
   return (
     <div>
-      <h2 style={{
-        fontSize: '2em',
-        padding: '1em'
-      }}>What's Hot</h2>
-      <div
-        className='hot-cities-section'
-        style={{
-          padding: '1em',
-          paddingBottom: '2em'
-        }}
-      >
-      {/* Render Cities */}
-      
-      {/* MediaCard */}
-      {cityList.map((city, index) => (
-        <Card key={index} sx={{
-          maxWidth: 545,
-          disableGutters: true,
-          fixed: true
-        }}>
-          <CardMedia
-            sx={{ height: 140 }}
-            image={city.image}
-            // title="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {city.name}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {city.country}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            {city.review.map((review)=> review.tags.slice(0,1).map((tag) => (
-              <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{tag}</span>
-            )))}
-            <Link to={`/cities/${city.id}`}>
-              <Button size="small" variant='outlined' >Learn More</Button>
-            </Link>
-          </CardActions>
-        </Card>
-      ))}
+      <div className='trending-title-container'>
+        <h2 className='bold trending-title'>Trending Cities</h2>
       </div>
+      <div className='hot-cities-section' style={{ padding: '1em', paddingBottom: '2em' }}>
+        {/* Render Cities */}
+        <Grid container spacing={6}>
+          {cityList.map((city, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={city.name}>
+              <Card sx={{ maxWidth: 345, m: 'auto' }}>
+                <CardMedia
+                  sx={{ height: 200 }}
+                  image={city.image}
+                  title={city.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {city.name}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    {city.country}
+                  </Typography>
+                </CardContent>
+                <CardActions sx={{ justifyContent: 'space-between', padding: '10px 16px', alignItems: 'center' }}>
+                  {city.review.flatMap((review) => review.tags.slice(0,2).map((tag) => (
+                    <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{tag}</span>
+                  )))}
+                  <Link to={`/cities/${city.id}`}>
+                    <Button size="small" variant='outlined'>Learn More</Button>
+                  </Link>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+
       {/* Review Section */}
       <div className='review-section'>
         <div className='review-title'>
